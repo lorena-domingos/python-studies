d2618cb2cb782e3d4a21a49b60d392b9018fabc9
Lorena Mendes <212278877+lorena-domingos@users.noreply.github.com>
2026-02-03 21:13:43 -0300

    feat: Completando alguns exercícios do nível 2

--- ex2.py
+++ ex2.py
@@ -29,3 +29,68 @@ def calculadora(valor1, operador, valor2):
         return valor1 - valor2
 
     return None
+
+# Retornar dia da semana com Match Case
+
+def dia_semana(valor):
+    match int(valor):
+        case 1:
+            return "Segunda"
+        case 2:
+            return "Terça"
+        case 3:
+            return "Quarta"
+        case 4:
+            return "Quinta"
+        case 5:
+            return "Sexta"
+
+    return None
+
+# Desafio FizzBuzz em Python
+
+def fizz_buzz(valor):
+    lista = []
+    for i in range(1, valor + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            lista.append("TeAmo")
+        elif i % 3 == 0:
+            lista.append("Te")
+        elif i % 5 == 0:
+            lista.append("Amo")
+        else:
+            lista.append(i)
+
+    return lista
+
+# fizz_buzz(10)
+
+# Nota Conceito
+
+def nota_conceito(nota):
+    if nota >= 9:
+        return "A"
+    elif nota >= 7:
+        return "B"
+    elif nota >= 5:
+        return "C"
+    else:
+        return "D"
+
+# Maior Valor
+
+def maior_valor(a, b, c):
+    return max(a, b, c)
+
+# Tabuada
+
+def tabuada(sequencia):
+    tab = []
+    contador = 0
+    for i in range(1, 11):
+        contador += 1
+        tab.append(f"{sequencia} * {contador} = {sequencia * contador}")
+    return tab
+
+# tabuada(2)
+
--- teste_ex2.py
+++ teste_ex2.py
@@ -12,3 +12,25 @@ def testando_calculadora():
     assert calculadora(2, "-", 2) == 0
     assert calculadora(2, "/", 2) == 1
     assert calculadora(2, "", 2) == "Inválido"
+
+def testando_semana():
+    assert dia_semana(1) == "Segunda"
+    assert dia_semana(2) == "Terça"
+    assert dia_semana(3) == "Sábado"
+
+def testando_fizz():
+    assert fizz_buzz(10) == [1, 2, "Te", 4, "Amo", "Te", 7, 8, "Te", "Amo"]
+
+def testando_nota_conceitual():
+    assert nota_conceito(2) == "D"
+    assert nota_conceito(6) == "C"
+    assert nota_conceito(8) == "B"
+    assert nota_conceito(10) == "A"
+
+def testando_maior_valor():
+    assert maior_valor(1, 2, 3) == 3
+    # assert maior_valor(1, 2, 3) == 2
+
+def testando_tabuada():
+    assert tabuada(2) == ['2 * 1 = 2', '2 * 2 = 4', '2 * 3 = 6', '2 * 4 = 8', '2 * 5 = 10', '2 * 6 = 12', '2 * 7 = 14', '2 * 8 = 16', '2 * 9 = 18', '2 * 10 = 20']
+
